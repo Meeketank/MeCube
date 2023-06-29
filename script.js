fbb8afbfcd2a33d06bef00883fb09256464e90f7
MEEKET KETAN TANK <86684823+Meeketank@users.noreply.github.com>
2023-06-29 12:50:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,16 +32,16 @@ function handleTouchMove(event) {
   if (!startX || !startY) return;
   
   const touch = event.touches[0];
-  const deltaX = touch.clientX - startX;
-  const deltaY = touch.clientY - startY;
+  const deltaX = touch.clientY - startX;
+  const deltaY = touch.clientX - startY;
   
   rotateY += deltaX * 0.5;
   rotateX -= deltaY * 0.5;
   
   rotateCube();
   
-  startX = touch.clientX;
-  startY = touch.clientY;
+  startX = touch.clientY;
+  startY = touch.clientX;
 }
 
 function handleTouchEnd() {
